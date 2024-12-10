099c5a5644bd2bed035b58b9caaa42e3b5a234d2
ReachableCEO <reachableceo@turnsys.com>
2024-12-10 16:35:21 -0600

    changes from first couple tests. we continue to iterate rapidly.

--- local/build/build-resume.sh
+++ local/build/build-resume.sh
@@ -87,24 +87,24 @@ export CandidateRateSheetRemote1099HourlyPrefer="\$78.90"
 ##################################################################
 
 
-readonly MO_PATH="bash ../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
-readonly BUILD_OUTPUT_DIR="$PipelineClientWorkingDir/build-output"
-readonly BUILD_TEMP_DIR="$PipelineClientWorkingDir/build-temp"
-readonly BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
-readonly BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
-readonly BUILDYAML_CANDIDATEINFOSHEET="$BUILD_TEMP_DIR/CandidateInfoSheet.yml"
+export MO_PATH="bash ../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
+export BUILD_OUTPUT_DIR="$PipelineClientWorkingDir/build-output"
+export BUILD_TEMP_DIR="$PipelineClientWorkingDir/build-temp"
+export BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
+export BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
+export BUILDYAML_CANDIDATEINFOSHEET="$BUILD_TEMP_DIR/CandidateInfoSheet.yml"
 
-CandidateInfoSheetMarkdownOutputFile="$BUILD_OUTPUT_DIR/CandidateInfoSheet.md"
-CandidateInfoSheetPDFOutputFIle="$BUILD_OUTPUT_DIR/CandidateInfoSheet.pdf"
+export CandidateInfoSheetMarkdownOutputFile="$BUILD_OUTPUT_DIR/CandidateInfoSheet.md"
+export CandidateInfoSheetPDFOutputFIle="$BUILD_OUTPUT_DIR/CandidateInfoSheet.pdf"
 
-JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.md"
-JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.pdf"
-JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.doc"
+export JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.md"
+export JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.pdf"
+export JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.doc"
 
-ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.md"
-ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.pdf"
-ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.doc"
+export ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.md"
+export ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.pdf"
+export ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.doc"
 
 # Call the build-pipeline-server in the vendored repository to produce output artifacts
 
-bash ../vendor/git.knownelement.com/reachableceo/MarkdownResume-Pipeline/build/build-pipeline-server.sh
+bash ../../vendor/git.knownelement.com/reachableceo/MarkdownResume-Pipeline/build/build-pipeline-server.sh
