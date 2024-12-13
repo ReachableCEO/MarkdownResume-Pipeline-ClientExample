f99dc2a112a704183fe3691e1d56524182c2f0a4
Charles N Wyble <reachableceo@turnsys.com>
2024-12-13 11:57:42 -0600

    Client server refactor

---
+++ local/build-output/MarkdownResume/job-board/Resume.md
@@ -0,0 +1,36 @@
+First Middle Last
+=====
+
+
+
+[ [Github Profile](https://www.github.com/) ] .  [ [Linkedin Profile](https://www.linkedin.com/) ] . [ candidate@domain.com ] . [ 1 123 456 7890 ] . [ Place 1/Place 2 ]
+ 
+## Career Highlights
+- Cool project 1.
+- cool project 2.
+\pagebreak
+ 
+## Employment History
+ 
+ 
+**job1 | Title of job 1 | January 1 1970 - January 6 1980**
+ 
+ 
+- Stuff
+- Things
+- Amazing project! 
+ 
+**job2 | Title of Job 2 | January 1 1970 - January 6 1980**
+ 
+ 
+- Stuff
+- Things
+- Amazing project! 
+\pagebreak
+ 
+## Skills
+ 
+|Skill|Experience|Skill Details|
+|---|---|---|
+|**skill1**|1 years|skill,1,details,and,stuff|
+|**skill2**|2 years|skill2,2,details,and,stuff,and,things|
---
+++ local/build/build-demo-markdown.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# This is a demo script for the MarkdownResumePipeline server
+# This script creates PDF/MSWord output from markdown/csv input
+
+
+############################################
+#Edit this file to reflect your information
+############################################
+
+souurce ./CandidateVariables.env
+
+####################################################
+####################################################
+####################################################
+#DO NOT CHANGE ANYTHING BELOW THIS LINE
+####################################################
+####################################################
+####################################################
+
+######################################################################################
+# Setup key variables that will be used by the build-pipeline-server-markdown.sh script
+######################################################################################
+
+readonly MO_PATH="bash ../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
+readonly BUILD_OUTPUT_DIR="../build-output/MarkdownResume/"
+readonly BUILD_TEMP_DIR="../build-temp/MarkdownResume"
+readonly BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
+readonly BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
+readonly BUILDYAML_CANDIDATEINFOSHEET="$BUILD_TEMP_DIR/CandidateInfoSheet.yml"
+
+CandidateInfoSheetMarkdownOutputFile="$BUILD_OUTPUT_DIR/CandidateInfoSheet.md"
+CandidateInfoSheetPDFOutputFIle="$BUILD_OUTPUT_DIR/CandidateInfoSheet.pdf"
+
+JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.md"
+JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.pdf"
+JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.doc"
+
+ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.md"
+ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submission//Resume.pdf"
+ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.doc"
+
+echo "Cleaning up from previous runs..."
+
+rm $BUILDYAML_CANDIDATEINFOSHEET
+rm $CandidateInfoSheetMarkdownOutputFile
+rm $CandidateInfoSheetPDFOutputFIle
+
+rm $BUILDYAML_JOBBOARD
+rm $JobBoardMarkdownOutputFile
+rm $JobBoardPDFOutputFile
+rm $JobBoardMSWordOutputFile
+
+rm $BUILDYAML_CLIENTSUBMISSION
+rm $ClientSubmissionMarkdownOutputFile
+rm $ClientSubmissionPDFOutputFile
+rm $ClientSubmissionMSWordOutputFile
+
+bash ../vendor/git.knownelement.com/reachableceo/MarkdownResume-Pipeline/build/build-pipeline-server-markdown.sh
