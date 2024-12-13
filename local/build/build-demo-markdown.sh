40afa5fd993e4c68e919f1aa6b941e8bc8a3ddce
Charles N Wyble <reachableceo@turnsys.com>
2024-12-13 12:05:57 -0600

    refactor

--- local/build/build-demo-markdown.sh
+++ local/build/build-demo-markdown.sh
@@ -8,7 +8,7 @@
 #Edit this file to reflect your information
 ############################################
 
-souurce ./CandidateVariables.env
+source "./CandidateVariables.env"
 
 ####################################################
 ####################################################
@@ -22,9 +22,9 @@ souurce ./CandidateVariables.env
 # Setup key variables that will be used by the build-pipeline-server-markdown.sh script
 ######################################################################################
 
-readonly MO_PATH="bash ../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
+readonly MO_PATH="bash ../../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
 readonly BUILD_OUTPUT_DIR="../build-output/MarkdownResume/"
-readonly BUILD_TEMP_DIR="../build-temp/MarkdownResume"
+readonly BUILD_TEMP_DIR="../build-temp/MarkdownResume/"
 readonly BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
 readonly BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
 readonly BUILDYAML_CANDIDATEINFOSHEET="$BUILD_TEMP_DIR/CandidateInfoSheet.yml"
@@ -33,7 +33,7 @@ CandidateInfoSheetMarkdownOutputFile="$BUILD_OUTPUT_DIR/CandidateInfoSheet.md"
 CandidateInfoSheetPDFOutputFIle="$BUILD_OUTPUT_DIR/CandidateInfoSheet.pdf"
 
 JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.md"
-JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.pdf"
+JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR//job-board/Resume.pdf"
 JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/Resume.doc"
 
 ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submission/Resume.md"
@@ -56,4 +56,4 @@ rm $ClientSubmissionMarkdownOutputFile
 rm $ClientSubmissionPDFOutputFile
 rm $ClientSubmissionMSWordOutputFile
 
-bash ../vendor/git.knownelement.com/reachableceo/MarkdownResume-Pipeline/build/build-pipeline-server-markdown.sh
+bash ../../vendor/git.knownelement.com/reachableceo/MarkdownResume-Pipeline/build/build-pipeline-server-markdown.sh
