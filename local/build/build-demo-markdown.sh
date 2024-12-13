7cd4b4089a0ae51d14fc7b8b4507548ad2c7946d
Charles N Wyble <reachableceo@turnsys.com>
2024-12-13 12:43:40 -0600

    more refactor.

--- local/build/build-demo-markdown.sh
+++ local/build/build-demo-markdown.sh
@@ -24,8 +24,10 @@ source "./CandidateVariables.env"
 ######################################################################################
 
 export MO_PATH="bash ../../vendor/git.knownelement.com/ExternalVendorCode/mo/mo"
+
 export BUILD_TEMP_DIR="$PipelineClientWorkingDir/build-temp/MarkdownResume"
 export BUILD_OUTPUT_DIR="$PipelineClientWorkingDir/build-output/MarkdownResume"
+
 export BUILDYAML_JOBBOARD="$BUILD_TEMP_DIR/JobBoard.yml"
 export BUILDYAML_CLIENTSUBMISSION="$BUILD_TEMP_DIR/ClientSubmission.yml"
 export BUILDYAML_CANDIDATEINFOSHEET="$BUILD_TEMP_DIR/CandidateInfoSheet.yml"
@@ -33,13 +35,13 @@ export BUILDYAML_CANDIDATEINFOSHEET="$BUILD_TEMP_DIR/CandidateInfoSheet.yml"
 export CandidateInfoSheetMarkdownOutputFile="$BUILD_OUTPUT_DIR/recruiter/CandidateInfoSheet.md"
 export CandidateInfoSheetPDFOutputFIle="$BUILD_OUTPUT_DIR/recruiter/CandidateInfoSheet.pdf"
 
-export JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.md"
-export JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.pdf"
-export JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/CharlesNWyble-Resume.doc"
+export JobBoardMarkdownOutputFile="$BUILD_OUTPUT_DIR/job-board/Candidate-Resume.md"
+export JobBoardPDFOutputFile="$BUILD_OUTPUT_DIR/job-board/Candidate-Resume.pdf"
+export JobBoardMSWordOutputFile="$BUILD_OUTPUT_DIR/job-board/Candidate-Resume.doc"
 
-export ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.md"
-export ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.pdf"
-export ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submit/CharlesNWyble-Resume.doc"
+export ClientSubmissionMarkdownOutputFile="$BUILD_OUTPUT_DIR/client-submit/Candidate-Resume.md"
+export ClientSubmissionPDFOutputFile="$BUILD_OUTPUT_DIR/client-submit/Candidate-Resume.pdf"
+export ClientSubmissionMSWordOutputFile="$BUILD_OUTPUT_DIR/client-submit/Candidate-Resume.doc"
 
 echo "Cleaning up from previous runs..."
 
